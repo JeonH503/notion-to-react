39cadfc450ab28405618e2ce744738be67b37c1f
JunHui Jeon <wjs4222@naver.com>
2023-04-07 17:57:16 +0900

    styled-component 적용

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,3 +1,4 @@
 export {default as paragraph} from './paragraph'
 export {default as heading} from './heading'
 export {default as divider} from './divider'
+export {default as code} from './code'
--- src/component/paragraph.tsx
+++ src/component/paragraph.tsx
@@ -1,13 +1,39 @@
-const Display_texts = (texts:any) => {
+import styled from 'styled-components'
+
+interface TextProps {
+    bold?:boolean;
+    italic?:boolean;
+    strikethrough?:boolean;
+    underline?:boolean;
+    color?:string
+}
+
+const Text = styled.span<TextProps>`
+    ${props => props.bold && 'font-weight: bold;'}
+    ${props => props.italic && 'font-style: italic;'}
+    ${
+        props => {
+            if(props.strikethrough || props.underline) {
+                // let deco = 
+                // return 
+            } else {
+                return ''
+            }
+        }
+    }
+    
+`
+
+const Block = (texts:any) => {
     return <>
-        {texts.map((e:any) => <span>{e.text.content}</span>)}
+        {texts.map((e:any) => <Text>{e.text.content}</Text>)}
     </>
 }
 
 function paragraph({texts}:any) {
     return(
         <div>
-            <Display_texts/>
+            <Block/>
         </div>
     )
 }
