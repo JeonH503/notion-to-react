899a50024ff7b3b0e7fbe8e5d012e13a764d59eb
jeon <wjs4222@naver.com>
2023-04-23 00:14:35 +0900

    스펠링 수정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
     :root {
-        --grey_background : rgb(241, 241, 239);
+        --gray_background : rgb(241, 241, 239);
         --brown_background : rgb(244, 238, 238);
         --orange_background : rgb(251, 236, 221);
         --yellow_background : rgb(251, 243, 219);
@@ -11,7 +11,7 @@ export default createGlobalStyle`
         --purple_background : rgba(244, 240, 247, 0.8);
         --pink_background : rgba(249, 238, 243, 0.8);
         --red_background : rgb(253, 235, 236);
-        --grey : rgb(120, 119, 116);
+        --gray : rgb(120, 119, 116);
         --brown : rgb(159, 107, 83);
         --orange : rgb(217, 115, 13);
         --yellow : rgb(203, 145, 47);
