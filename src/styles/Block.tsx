5873b181b50d3bb5676cf8f759e00ff2fe50386c
jeon <wjs4222@naver.com>
2023-05-20 18:49:31 +0900

    block min-hieght 추가

--- src/styles/Block.tsx
+++ src/styles/Block.tsx
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 export default styled.div<{color?:string}>`
     ${props => props.color && props.color.length > 11 ? `background:var(--${props.color})` : `color:(--${props.color})`};
     margin:5px 0;
+    min-height:30px;
 `
 
